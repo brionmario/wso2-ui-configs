263a41e5e90fb55d905049db60d4345784b6fd1d
Brion <info@brionmario.com>
2022-11-28 20:06:04 +0530

    chore(eslint): add `recommended` eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -16,25 +16,72 @@
  * under the License.
  */
 
- 'use strict';
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
 
 /**
  * @fileoverview Entry point for the ESLint config.
  */
 
+// Try to resolve a `tsconfig.json` file to avoid asking the user to provide one.
+const tsConfig = fs.existsSync('tsconfig.json')
+  ? path.resolve('tsconfig.json')
+  : fs.existsSync('types/tsconfig.json')
+    ? path.resolve('types/tsconfig.json')
+    : undefined;
+
 module.exports = {
-  extends: [
-    require.resolve('./base'),
-    'airbnb',
-    'airbnb-typescript'
-  ],
+  extends: [require.resolve('./base'), 'airbnb'],
+  // NOTE: When adding rules here, you need to make sure they are compatible with
+  // `typescript-eslint`, as some rules such as `no-array-constructor` aren't compatible.
+  rules: {
+    // We need prop spreading when writing wrapper components.
+    // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-props-no-spreading.md
+    'react/jsx-props-no-spreading': 'off',
+    // We prefer to write functional components as arrow-functions.
+    // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/function-component-definition.md
+    'react/function-component-definition': [
+      'error',
+      { namedComponents: 'arrow-function' },
+    ],
+    // JSX can be in `mdx` files as well.
+    // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.jsx', '.jsx', '.mdx'] },
+    ],
+    // Allow dev dependencies to be used in test specs and storybook files.
+    // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/no-extraneous-dependencies.md
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.test.?(j|t)s',
+          '**/*.spec.?(j|t)s',
+          '**/*.stories.?(j|t)s?(x)',
+          '**/*.stories.mdx',
+        ],
+      },
+    ],
+    // Enforces sorting object properties in alphabetical order for readability.
+    // https://eslint.org/docs/latest/rules/sort-keys
+    'sort-keys': [
+      'error',
+      'asc',
+      { caseSensitive: true, natural: false, minKeys: 2 },
+    ],
+  },
   overrides: [
     {
       files: ['**/*.ts?(x)'],
+      extends: ['airbnb-typescript', 'plugin:typescript-sort-keys/recommended'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
         ecmaVersion: 2018,
         sourceType: 'module',
+        project: tsConfig,
         ecmaFeatures: {
           jsx: true,
         },
@@ -44,10 +91,9 @@ module.exports = {
       plugins: ['@typescript-eslint'],
       // If adding a typescript-eslint version of an existing ESLint rule,
       // make sure to disable the ESLint rule here.
-      rules: {},
+      rules: {
+        "react/default-props-match-prop-types": 'off'
+      },
     },
   ],
-  // NOTE: When adding rules here, you need to make sure they are compatible with
-  // `typescript-eslint`, as some rules such as `no-array-constructor` aren't compatible.
-  rules: {},
 };
