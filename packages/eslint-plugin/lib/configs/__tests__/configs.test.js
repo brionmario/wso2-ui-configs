6c3ca2318d0902089b8138537dc85f26223d1665
Brion <info@brionmario.com>
2022-12-06 17:18:17 +0530

    chore(eslint): fix unit tests

--- packages/eslint-plugin/lib/configs/__tests__/configs.test.js
+++ packages/eslint-plugin/lib/configs/__tests__/configs.test.js
@@ -1,15 +1,22 @@
-const config = require('../../index');
 const fs = require('fs');
 const assert = require('assert');
 const path = require('path');
+const config = require('../../index');
 
 describe('check shareable config', () => {
   it('correctly exports every config from the module', () => {
+    const KNOWN_FILES_TO_IGNORE = ['__tests__'];
     const exportedConfigs = new Set(Object.keys(config.configs));
     const files = new Set(
       fs
-        .readdirSync('../')
-        .map((f) => path.basename(f, path.extname(f)))
+        .readdirSync(path.resolve(path.join(__dirname, '..')))
+        .map((f) => {
+          if (KNOWN_FILES_TO_IGNORE.includes(f)) {
+            return undefined;
+          }
+
+          return path.basename(f, path.extname(f));
+        }).filter(Boolean),
     );
     assert.deepEqual(files, exportedConfigs);
   });
