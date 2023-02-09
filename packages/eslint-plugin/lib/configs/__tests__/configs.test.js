27de3d37a97801704a42e407342eca01dfd00ff6
Brion <info@brionmario.com>
2023-02-10 00:05:05 +0530

    fix(eslint): fix linter

--- packages/eslint-plugin/lib/configs/__tests__/configs.test.js
+++ packages/eslint-plugin/lib/configs/__tests__/configs.test.js
@@ -16,8 +16,8 @@
  * under the License.
  */
 
-const fs = require('fs');
 const assert = require('assert');
+const fs = require('fs');
 const path = require('path');
 const config = require('../../index');
 
